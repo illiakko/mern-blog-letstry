78d4acc9c3575dc14073a9416f8a32e14c02e8a6
Illia Kozachenko <ilya.s.kozachenko@gmail.com>
2022-08-03 23:36:19 +0300

    Post edit added, comments added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors'
+import fileUpload from 'express-fileupload';
 
 import authRoute from './routes/auth.js'
+import postRoute from './routes/posts.js'
 
 const app = express()
 dotenv.config()
@@ -13,7 +15,9 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
 app.use(cors())
+app.use(fileUpload())
 app.use(express.json())
+app.use(express.static('uploads'))
 
 app.get('/', (req, res) => {
     res.json({ message: "Hello from server!" })
@@ -23,6 +27,7 @@ app.get('/', (req, res) => {
 // http://localhost:3002
 
 app.use('/api/auth', authRoute)
+app.use('/api/posts', postRoute)
 
 async function start() {
 
